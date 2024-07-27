a12a25285ee498fa0568cfaa9fcc2e07b1f57d6d
= <=>
2024-07-27 20:53:17 +0800

    .

---
+++ main.js
@@ -0,0 +1,8 @@
+import player from "./player.js";
+
+window.onload = function () {
+    const game = document.getElementById("game");
+    const ctx = game.getContext("2d");
+    game.width = 800;
+    game.height = 600;
+}
---
+++ player.js
@@ -0,0 +1,8 @@
+const player = {
+    size: 50,
+    color: "yellow",
+    x: 0,
+    y: 0,
+}
+
+export default player;
