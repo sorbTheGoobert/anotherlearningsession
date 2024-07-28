51d17f277f3b8fc257755bd5cff75b8c79e3c909
= <=>
2024-07-28 16:58:58 +0800

    made a little slipper with w and s but not able to turn the player yet

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 import player from "./player.js";
+const game = document.getElementById("game");
+const ctx = game.getContext("2d");
 
-window.onload = function () {
-    const game = document.getElementById("game");
-    const ctx = game.getContext("2d");
-    game.width = 800;
-    game.height = 600;
+function init() {
+    player.x = (boardInfo.width - player.size) / 2;
+    player.y = (boardInfo.height - player.size) / 2;
+    game.width = boardInfo.width;
+    game.height = boardInfo.height;
+    boardInfo.frame();
 }
+
+const boardInfo = {
+    width: 800,
+    height: 600,
+    color: "black",
+    frame: function () {
+        //clear scene
+        ctx.clearRect(0, 0, boardInfo.width, boardInfo.height);
+        //update player position
+        player.update();
+        //draw frame
+        ctx.fillStyle = boardInfo.color;
+        ctx.fillRect(0, 0, boardInfo.width, boardInfo.height);
+        ctx.fillStyle = player.color;
+        ctx.fillRect(player.x, player.y, player.size, player.size);
+        requestAnimationFrame(boardInfo.frame);
+    },
+}
+
+init();
--- player.js
+++ player.js
@@ -1,8 +1,37 @@
+
 const player = {
-    size: 50,
-    color: "yellow",
+    size: 25,
+    color: "white",
     x: 0,
     y: 0,
+    accelaration: 2,
+    speed: 0,
+    angle: 0,
+    turnSpeed: 10,
+    move: function(event) {
+        console.log(event);
+        switch(event.code){
+            case "KeyW":
+                player.speed -= player.accelaration * 0.2;
+                break;
+            case "KeyS":
+                player.speed += player.accelaration * 0.2;
+                break;
+            case "KeyA":
+                player.angle -= player.turnSpeed;
+                console.log(player.angle);
+                break;
+            case "KeyD":
+                player.angle += player.turnSpeed;
+                console.log(player.angle);
+                break;
+        }
+    },
+    update: function() {
+        player.y = (player.accelaration * 0.2 * 0.2) + player.speed * 0.2 + player.y;
+    }
 }
+
+window.addEventListener("keypress", player.move);
 
 export default player;
